49945f1e33cb60a9d2aa7540354d09dc6ec46766
JoLiLol <149590155+JoLiLolpro@users.noreply.github.com>
2025-02-22 00:39:52 +0100

    Add files via upload

--- WPF .NET 6/Breath of the Wild Multiplayer/MainWindow.xaml.cs
+++ WPF .NET 6/Breath of the Wild Multiplayer/MainWindow.xaml.cs
@@ -26,6 +26,22 @@ namespace Breath_of_the_Wild_Multiplayer
             client = new DiscordRpcClient("1339782150471290904");
             client.Initialize();
         }
+
+        public static void updateRPC(string Details, string State)
+        {
+            client.SetPresence(new RichPresence()
+            {
+                Details = Details,
+                State = State,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "image_big",
+                    LargeImageText = $"V{MainWindow.VERSION} by The Lon Lon Ranch",
+                    //SmallImageKey = "little_image",
+                    //SmallImageText = "Text little_image",
+                }
+            });
+        }
     }
 
     public partial class MainWindow : Window
